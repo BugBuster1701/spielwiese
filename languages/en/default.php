4b47981ee4ec0d244540178ab6e9e35dcf6934fc
Glen Langer <contao.modul@glen-langer.de>
2013-10-12 01:34:43 +0200

    --

---
+++ languages/en/default.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/spielwiese/language/en/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-05-12T22:05:44+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['MSC']['tl_spielwiese']['id']      = 'ID';
+$GLOBALS['TL_LANG']['MSC']['tl_spielwiese']['name']    = 'Name';
+$GLOBALS['TL_LANG']['MSC']['tl_spielwiese']['picture'] = 'Picture';
