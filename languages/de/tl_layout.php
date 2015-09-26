2768012ecfd142a385bea8e11203476a775d6c5f
Glen Langer <contao.modul@glen-langer.de>
2015-09-27 00:03:03 +0200

    Test

---
+++ languages/de/tl_layout.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/spielwiese/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2015-05-11T20:37:54+02:00
+ */
+
+$GLOBALS['TL_LANG']['tl_layout']['fallback']['0']                            = 'darf nicht angepasst werden';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1120-16-pixel-responsive.css']['0']   = '16-Spalten Grid, Pixel, 1120px, Responsive Teil';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1120-16-pixel-responsive.css']['1']   = 'Responsive Teil für das 16-Spalten Pixel Grid mit 1120px Breite';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1120-16-pixel.css']['0']              = '16-Spalten Grid, Pixel, 1120px';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1120-16-pixel.css']['1']              = 'Erzeugt ein 16-Spalten Pixel Grid mit 1120px Breite, das mittels der CSS-Klassen "grid1" bis "grid16" sowie "offset1" bis "offset15" gesteuert wird.';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1140-16-percent-responsive.css']['0'] = '16-Spalten Grid, Prozent, 1140px, Responsive Teil';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1140-16-percent-responsive.css']['1'] = 'Responsive Teil für das 16-Spalten Prozent Grid mit 1140px Breite';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1140-16-percent.css']['0']            = '16-Spalten Grid, Prozent, 1140px';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1140-16-percent.css']['1']            = 'Erzeugt ein 16-Spalten Prozent Grid mit 1140px Breite, das mittels der CSS-Klassen "grid1" bis "grid16" sowie "offset1" bis "offset15" gesteuert wird.';
+
---
+++ languages/en/tl_layout.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/contao-grid_16_columns/language/en/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-06-08T22:51:20+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_layout']['grid-1120-16-pixel-responsive.css']['0']   = '16-column  grid, pixel, 1120px, responsive part';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1120-16-pixel-responsive.css']['1']   = 'Responsive part for the 16-column grid, pixel, 1120px';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1120-16-pixel.css']['0']              = '16-column  grid, pixel, 1120px';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1120-16-pixel.css']['1']              = 'Adds a 16-column pixel grid with 1120px, that is triggered by the CSS classes "grid1" to "grid16" and "offset1" to "offset15".';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1140-16-percent-responsive.css']['0'] = '16-column grid, percent, 1140px, responsive part';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1140-16-percent-responsive.css']['1'] = 'Responsive part for the 16-column grid, percent, 1140px';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1140-16-percent.css']['0']            = '16-column grid, percent, 1140px';
+$GLOBALS['TL_LANG']['tl_layout']['grid-1140-16-percent.css']['1']            = 'Adds a 16-column percental grid with 1140px, that is triggered by the CSS classes "grid1" to "grid16" and "offset1" to "offset15".';
+
+$GLOBALS['TL_LANG']['tl_layout']['fallback']['0'] = 'no css test';
